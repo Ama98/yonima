bd784b274cb1ddcb5ca260acef57bd075ea410b3
mballo <amadou.mballo1992@gmail.com>
2020-01-10 05:52:31 +0000

    edit 1

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Role;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+$role = new Role();
+$role->setLibelle("ADMIN_SYS");
+$manager->persist($role);
+
+$user = new User();
+$user->setUsername("supadmin");
+$user->setPassword($this->encoder->encodePassword($user, "system"));
+$user->setRoles(array("ROLE_".$role->getLibelle()));
+$user->setIsActive(true);
+$user->setProfil($role);
+
+$manager->persist($user);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Entity;
+
+use Serializable;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ApiResource()
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+    * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+    // public function __construct($profil = null, $username)
+    // {
+    //     $this->isActive = true;
+    //     $this->username = $username;
+    //     $this->profil = $profil;
+    // }
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $profil;
+    /**
+     * @SerializedName("password")
+     */
+    private $plainPassword;
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+    public function setPlainPassword(string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getProfil(): ?Role
+    {
+        return $this->profil;
+    }
+
+    public function setProfil(?Role $profil): self
+    {
+        $this->profil = $profil;
+
+        return $this;
+    }
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    public function serialize()
+    {
+        return serialize(array(
+            // ...
+            $this->isActive
+        ));
+    }
+    public function unserialize($serialized)
+    {
+        list (
+            // ...
+            $this->isActive
+        ) = unserialize($serialized);
+    }
+}
